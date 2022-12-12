3d7c947e3512b3c19d44b3e5a64df194fc495530
Daria <72020978+kkkovalllchuk@users.noreply.github.com>
2022-12-12 18:56:51 +0200

    Delete function.sql

--- function.sql
+++
@@ -1,8 +0,0 @@
-DROP PROCEDURE IF EXISTS insert_main(integer, varchar,varchar);
-CREATE OR REPLACE PROCEDURE insert_main(m_id_1 integer, title_1 varchar,author_1 varchar)
-LANGUAGE 'plpgsql'
-AS $$
-BEGIN
-    INSERT INTO orders(m_id, title, author) VALUES (m_id_1, title_1, author_1);
-END;
-$$;
